04bfc1c238f129ad85a1dbafe13d71e2b78b3f59
MrPipiKado <ammamm23102310@gmail.com>
2020-10-30 00:05:24 +0200

    Backend for education acc added

--- provider.tf
+++ provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "finalmrpipikado"
+    key = "final/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "Final"
+    encrypt = true
+  }
 }
 
 # Configure the AWS Provider
